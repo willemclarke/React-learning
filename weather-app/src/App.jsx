e2327fbc4836ea7983ef361cbd69a6220a1c34ef
Willem Clarke <willem.2@hotmail.com>
2020-01-03 15:22:07 +1000

    sixth commit - sorting out weather API

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -7,7 +7,34 @@ import "./sass/app.scss";
 import TopSection from "./components/top/index";
 import BottomSection from "./components/bottom/index";
 
+import axios from "axios";
+
+const WEATHER_KEY = "f4406833aa13ba04e0ea552ad5454c6b";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: "London",
+      forecastDays: 5
+    };
+  }
+
+  componentDidMount() {
+    const { cityName, forecastDays } = this.state;
+    const URL = `http://api.weatherstack.com/forecast?access_key=${WEATHER_KEY} &query=${cityName} &forecast_days=${forecastDays}`;
+    axios
+      .get(URL)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        if (err) {
+          console.error("Failed to recieve API data ", err);
+        }
+      });
+  }
+
   render() {
     return (
       <div className="app-container">
